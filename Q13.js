cfd63ec099bce636fe506d254e0038e052d8993e
Sheikh-Muhammad-Sadiq <162907621+Sheikh-Muhammad-Sadiq@users.noreply.github.com>
2024-03-17 03:50:20 -0700

    Add files via upload
    Q 13 Node.js of 45 Assignments

---
+++ Q13.js
@@ -0,0 +1,7 @@
+"use strict";
+//13. Your Own Array: Think of your favorite mode of tranportaion, such as motorsycle or a car, and 
+// make a list  that stores several examples. Use your list
+// to print a series of statement about these Items, such as "I whould like to own a 
+// Honda motorcycle."
+let vehichles = ["Honda motorcycle", "Honda Civic", "Honda City", "Audi"];
+vehichles.map((items) => console.log(`I whould like to own a ${items}`));
---
+++ Q13.ts
@@ -0,0 +1,8 @@
+//13. Your Own Array: Think of your favorite mode of tranportaion, such as motorsycle or a car, and 
+// make a list  that stores several examples. Use your list
+// to print a series of statement about these Items, such as "I whould like to own a 
+// Honda motorcycle."
+
+let vehichles: string[] = ["Honda motorcycle", "Honda Civic", "Honda City", "Audi"]
+
+vehichles.map((items) => console.log(`I whould like to own a ${items}`));
